34447b162a46ab552630bbf6ee707f30395ebf0f
007jiggy <0raskenddys@gmail.com>
2024-06-23 08:56:01 +0000

    jiggy-init

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "JIGGYCHAIN"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "JIGGY"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JIG"
 )
 
 var ID ids.ID
